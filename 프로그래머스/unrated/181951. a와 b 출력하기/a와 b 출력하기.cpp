c3f4835e0db91d46c3667d37ee7deaae673dbe55
Jinaen <68771741+fire-long@users.noreply.github.com>
2023-10-05 07:46:57 +0900

    [unrated] Title: a와 b 출력하기, Time: 1.52 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    if ((-100000<= a <=100000) & (-100000<= b <=100000))
+        cout<<"a = "<< a << endl;
+        cout<<"b = "<< b << endl;
+    return 0;
+}
