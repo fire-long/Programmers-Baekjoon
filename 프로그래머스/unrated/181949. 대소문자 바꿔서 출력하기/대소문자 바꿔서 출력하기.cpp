92a80eb6a1b0965016007411d05148f9ba4017e5
Jinaen <68771741+fire-long@users.noreply.github.com>
2023-10-09 17:36:43 +0900

    Update 대소문자 바꿔서 출력하기.cpp

--- 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -8,6 +8,7 @@ int main(void) {
     cin >> str;
     if (1<=str.length()<=20)
         for (char &c : str){
+            //&c는 참조하는 것으로, 변수에 대한 별칭임.
             if (islower(c)){
                 c = toupper(c);
             }else if (isupper(c)){
