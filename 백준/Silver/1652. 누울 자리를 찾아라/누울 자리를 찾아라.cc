2d14fec8fa0fb6e51411d0771e8b678e6769f0c8
Jinaen <68771741+fire-long@users.noreply.github.com>
2023-10-09 18:31:34 +0900

    Update 누울 자리를 찾아라.cc

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.cc
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.cc
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-
+// 사용자로부터 정사각형 방의 크기(N)와 각 위치의 상태를 입력받아, 가로로 누울 수 있는 자리와 세로로 누울 수 있는 자리의 수를 세는 프로그램
 int main() {
     int N;
     cin >> N;
@@ -14,8 +14,8 @@ int main() {
         }
     }
 
-    int horizontal_count = 0;
-    int vertical_count = 0;
+    int horizontal_count = 0; // 가로로 누울 수 있는 자리의 수
+    int vertical_count = 0; //세로로 누울 수 있는 자리의 수
 
     // 가로로 누울 수 있는 자리 검사
     for (int i = 0; i < N; ++i) {
@@ -27,7 +27,7 @@ int main() {
                 if (empty_count >= 2) {
                     horizontal_count++;
                 }
-                empty_count = 0;
+                empty_count = 0; //빈자리가 아닌 다른 문자가 나타날 때 0으로 초기화
             }
         }
         if (empty_count >= 2) {
