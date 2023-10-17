eb47a06fb5a5ac2fe4725362a32f27ea8112bdc3
Jinaen <68771741+fire-long@users.noreply.github.com>
2023-10-17 17:33:45 +0900

    Update 부등호.py

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -1,6 +1,7 @@
 from itertools import permutations
 
 def check(inequalities, num):
+    #부등호 조건 확인
     for i in range(len(inequalities)):
         if inequalities[i] == '<' and num[i] > num[i+1]:
             return False
@@ -12,7 +13,9 @@ def find_max_min_numbers(k, inequalities):
     max_num = None
     min_num = None
     for perm in permutations('0123456789', k+1):
-        num = ''.join(perm)
+        #'0123456789' 문자열에서 'k+1'개의 숫자를 가지는 모든 순열을 생성함.
+        num = ''.join(perm) #문자열 리스트를 하나의 문자열로 결합함.
+        
         if check(inequalities, num):
             if max_num is None or int(num) > int(max_num):
                 max_num = num
@@ -21,7 +24,7 @@ def find_max_min_numbers(k, inequalities):
     return max_num, min_num
 
 # 입력 처리
-k = int(input())
+k = int(input()) #k : 입력으로 들어온 부등호 개수
 inequalities = input().split()
 
 # 최댓값과 최솟값 찾기
