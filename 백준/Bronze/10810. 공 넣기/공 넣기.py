613ccb6c22eb0d913b96647b1ccdf67065b73bd5
Jinaen <68771741+fire-long@users.noreply.github.com>
2024-05-05 15:05:31 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -5,5 +5,6 @@ input_list = [0]*N;#0으로 초기화.
 for x in range(M):
     i, j, k = map(int, sys.stdin.readline().split())
     input_list[i-1:j] = [k]*(j-(i-1))
-for x in range(0,N):
-    print(input_list[x],end=' ')    
+#for x in range(0,N):
+#    print(input_list[x],end=' ')    
+print(*input_list)
