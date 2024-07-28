c7c0a5653a720637a8911960871385c50dc4c907
Jinaen <68771741+fire-long@users.noreply.github.com>
2024-07-28 18:48:03 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -2,5 +2,5 @@ import sys
 white_numbers = list(map(int, sys.stdin.readline().split()))
 standard_numbers = [1, 1, 2, 2, 2, 8]
 
-for i in range(len(white_numbers)):
-    print(standard_numbers[i]-white_numbers[i], end=' ')
+for s, w in zip(standard_numbers, white_numbers):
+    print(s-w, end=' ')
