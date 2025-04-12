670abf80d1f07b411bfb298ce005435a9f40bb62
Jinaen <68771741+fire-long@users.noreply.github.com>
2025-04-12 20:37:14 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.py
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.py
@@ -1,10 +1,4 @@
 import sys
-fhts = int(sys.stdin.readline().strip())
-fstp = int(sys.stdin.readline().strip())
-fpth = int(sys.stdin.readline().strip())
-fhth = int(sys.stdin.readline().strip())
-
-total_sec = fhts+fstp+fpth+fhth
-
+total_sec = sum([int(sys.stdin.readline().strip()) for _ in range(4)])
 print(total_sec//60)
 print(total_sec%60)
