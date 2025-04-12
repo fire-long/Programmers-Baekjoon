22543ef575682391552faefe7c88bf3f29d45b08
Jinaen <68771741+fire-long@users.noreply.github.com>
2025-04-12 20:51:08 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.py
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.py
@@ -1,4 +1,5 @@
 import sys
 L, P = map(int, sys.stdin.readline().strip().split(' '))
-n1, n2, n3, n4, n5 = map(int, sys.stdin.readline().strip().split(' '))
-print(n1-L*P, n2-L*P, n3-L*P, n4-L*P, n5-L*P)
+news = list(map(int, sys.stdin.readline().strip().split(' ')))
+for i in news:
+    print(i - L*P, end=' ')
