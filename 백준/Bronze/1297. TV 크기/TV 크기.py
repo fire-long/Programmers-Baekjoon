7704e5bd8935e6d82a92e30b7d201f7b381ba794
Jinaen <68771741+fire-long@users.noreply.github.com>
2025-04-14 20:52:19 +0900

    [Bronze II] Title: TV 크기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1297. TV 크기/TV 크기.py
+++ 백준/Bronze/1297. TV 크기/TV 크기.py
@@ -1,6 +1,6 @@
-import sys, math
-D, H, W = map(int,sys.stdin.readline().rstrip().split())
+import sys
+D, H, W = map(int, sys.stdin.readline().rstrip().split())
 
-d = math.sqrt(H**2+W**2)
+d = (H**2+W**2)**0.5
 ratio = D/d
 print(int(H*ratio), int(W*ratio))
