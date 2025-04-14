e0939df52470bd687418d0686ae03672cf241daa
Jinaen <68771741+fire-long@users.noreply.github.com>
2025-04-14 15:33:41 +0900

    [Bronze III] Title: 윷놀이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.py
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -1,11 +1,13 @@
 import sys
-res = []
 for _ in range(3):
-    res.append(sys.stdin.readline().rstrip().count('0'))
-
-for i in range(3):
-    if res[i] == 1: print("A")
-    elif res[i] == 2: print("B")
-    elif res[i] == 3: print("C")
-    elif res[i] == 4: print("D")
-    elif res[i] == 0:print("E")
+    res = list(map(int, sys.stdin.readline().rstrip().split()))
+    if res.count(0) == 1:
+        print("A")
+    elif res.count(0) == 2:
+        print("B")
+    elif res.count(0) == 3:
+        print("C")
+    elif res.count(0) == 4:
+        print("D")
+    elif res.count(0) == 0:
+        print("E")
