c30a89f6af271a37714644c381bddfc8b8c77fbe
Jinaen <68771741+fire-long@users.noreply.github.com>
2025-05-09 21:56:44 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -2,12 +2,11 @@ import sys
 N = int(sys.stdin.readline().rstrip())
 info = [list(map(int, sys.stdin.readline().rstrip().split())) 
         for _ in range(N)]
-answer = []
+
 for i in range(len(info)):
-    count = 0
+    big = 0
     for j in range(len(info)):
         if i == j: continue
         if info[i][0] < info[j][0] and info[i][1] < info[j][1]:
-            count += 1
-    answer.append(count+1)    
-print(*answer)
+            big += 1
+    print(big+1, end=" ")
