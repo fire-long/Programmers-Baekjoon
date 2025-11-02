8087cbb97e468b4d311aa7f2eb9b30c1e761ff3a
Jinaen <68771741+fire-long@users.noreply.github.com>
2025-11-03 08:53:41 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.00 ms, Memory: 9.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -0,0 +1,11 @@
+def solution(s):    
+    answer = True
+    
+    s = s.lower()
+    p_cnt = s.count('p')
+    y_cnt = s.count('y')
+    
+    if p_cnt != y_cnt:
+        answer = False
+
+    return answer
