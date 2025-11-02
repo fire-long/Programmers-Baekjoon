6d831ad14a346ecc88a600ff000d4a1560d2ec91
Jinaen <68771741+fire-long@users.noreply.github.com>
2025-11-03 08:38:50 +0900

    [level 1] Title: 최소직사각형, Time: 2.68 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,11 @@
+def solution(sizes):
+    max_x = 0
+    max_y = 0
+    for i in range(len(sizes)):
+        if sizes[i][0] > sizes[i][1]:
+            sizes[i][0], sizes[i][1] = sizes[i][1], sizes[i][0]
+        if sizes[i][0] > max_x:
+            max_x = sizes[i][0]    
+        if sizes[i][1] > max_y:
+            max_y = sizes[i][1]
+    return max_x * max_y
