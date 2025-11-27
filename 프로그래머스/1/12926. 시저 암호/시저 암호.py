212b6b54bf873e0444fd12f423f59aa6609c245a
Jinaen <68771741+fire-long@users.noreply.github.com>
2025-11-27 17:44:28 +0900

    [level 1] Title: 시저 암호, Time: 1.77 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -2,16 +2,9 @@ def solution(s, n):
     answer = []
     for x in s:
         if x.isalpha():
-            if x.isupper():
-                if ord(x)+n > ord('Z'):
-                    answer.append(chr((ord(x)+n)-ord('Z')+ord('A')-1)) #65~90
-                else:
-                    answer.append(chr(ord(x)+n))
-            else:
-                if ord(x)+n > ord('z'):
-                    answer.append(chr((ord(x)+n)-ord('z')+ord('a')-1)) #97~122 
-                else:
-                    answer.append(chr(ord(x)+n))
+            base = ord('A') if x.isupper() else ord('a')
+            offset = (ord(x) - base + n) % 26
+            answer.append(chr(base + offset))
         else:
             answer.append(x)
     answer = ''.join(answer)
